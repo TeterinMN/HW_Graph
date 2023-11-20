fe716dd74458c36c25063a00427da613aea30ba7
TeterinMN <Scorpion.TMN@ya.ru>
2023-11-20 10:58:37 +0700

    New commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10 + ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+one_hot_encoded = pd.concat([pd.Series(1, index=data.index, name='whoAmI'),
+                             pd.get_dummies(data['whoAmI'])], axis=1)
+data_one_hot = pd.concat([data, one_hot_encoded], axis=1)
+data_one_hot = data_one_hot.drop('whoAmI', axis=1)
+
+data_one_hot.head()
+print(data)
+print()
+print(data_one_hot)
