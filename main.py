5fe3d0f4fa2ff2b88f50d7e5570b20ca7d998f2e
TeterinMN <Scorpion.TMN@ya.ru>
2023-11-20 11:05:05 +0700

    Внес корректировку в код

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ data = pd.DataFrame({'whoAmI': lst})
 
 one_hot_encoded = pd.concat([pd.Series(1, index=data.index, name='whoAmI'),
                              pd.get_dummies(data['whoAmI'])], axis=1)
+one_hot_encoded = one_hot_encoded.astype(int)
 data_one_hot = pd.concat([data, one_hot_encoded], axis=1)
 data_one_hot = data_one_hot.drop('whoAmI', axis=1)
 
